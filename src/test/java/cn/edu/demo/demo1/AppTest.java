05eec0f82155561a97208188cd431770e81a5277
admin <admin@192.168.207.1>
2017-10-11 21:21:48 +0800

    testABC

---
+++ src/test/java/cn/edu/demo/demo1/AppTest.java
@@ -0,0 +1,26 @@
+package cn.edu.demo.demo1;
+
+import org.testng.annotations.Test;
+
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void testApp()
+    {
+    	System.out.println("testNGtestNGtestNG");
+        //assertTrue( true );
+    }
+}
